ed3670f50b564493185bcd65daf42b3230099a6f
ujaann <ujanst@gmail.com>
2024-11-26 12:23:29 +0545

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:first_assignment/view/dashboard_view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DashboardView();
+  }
+}
