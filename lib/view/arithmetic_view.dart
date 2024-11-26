d650ad02aba3e133afb5402073015ebe77dc2b81
ujaann <ujanst@gmail.com>
2024-11-26 12:38:23 +0545

    Added multiplication, addition, subtraction functionality

---
+++ lib/view/arithmetic_view.dart
@@ -0,0 +1,69 @@
+import 'package:first_assignment/widgets/gap.dart';
+import 'package:flutter/material.dart';
+
+class ArithmeticView extends StatefulWidget {
+  const ArithmeticView({super.key});
+
+  @override
+  State<ArithmeticView> createState() => _ArithmeticViewState();
+}
+
+class _ArithmeticViewState extends State<ArithmeticView> {
+  int first = 0;
+  int second = 0;
+  int result = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          TextField(
+            onChanged: (value) => first = int.parse(value),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "First number:",
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          gap8y,
+          TextField(
+            onChanged: (value) => second = int.parse(value),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Second number:",
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          gap8y,
+          Text("Result: $result",style: const TextStyle(fontSize: 22),),
+          gap8y,
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  result = first + second;
+                });
+              },
+              child: const Text("Addition")),
+          gap8y,
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  result = first - second;
+                });
+              },
+              child: const Text("Subtraction")),
+          gap8y,
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  result = first * second;
+                });
+              },
+              child: const Text("Multiplication")),
+        ],
+      )),
+    );
+  }
+}
